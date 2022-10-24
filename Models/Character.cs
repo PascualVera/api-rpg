fbcb867535516d24bc1524c5e8c2cb0a51e166b1
PascualVera <pascualveradev@gmail.com>
2022-10-24 14:43:21 +0200

    Auth service added

--- Models/Character.cs
+++ Models/Character.cs
@@ -5,19 +5,21 @@ using System.Threading.Tasks;
 
 namespace dotnet_rpg.Models
 {
-    public class Character
-    {
-        public int Id {get; set; }
+  public class Character
+  {
+    public int Id { get; set; }
 
-        public string Name {get; set;} = "Toby";
-        public int Hitpoints {get; set;} = 100;
+    public string Name { get; set; } = "Toby";
+    public int Hitpoints { get; set; } = 100;
 
-        public int Strength {get; set;} = 10;
+    public int Strength { get; set; } = 10;
 
-        public int Defense {get; set;} = 10;
+    public int Defense { get; set; } = 10;
 
-        public int Intelligence {get; set;} = 100; 
+    public int Intelligence { get; set; } = 100;
 
-        public RpgClass Class {get; set;} = RpgClass.Mage;
-    }
+    public RpgClass Class { get; set; } = RpgClass.Mage;
+
+    public User? User { get; set; }
+  }
 }
